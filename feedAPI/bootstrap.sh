3f25310511f2772eab2ec1508e6b1bb6845a32d6
Ben Dum <bedumont@ulb.ac.be>
2023-04-28 18:13:43 +0200

    Added path context to bootstrapping script

--- feedAPI/bootstrap.sh
+++ feedAPI/bootstrap.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-export FLASK_APP=./feedAPI/index.py
-. ./.venv/bin/activate
+cd /home/ben/code/feedAPI-project/feedAPI
+export FLASK_APP=./index.py
+. ../.venv/bin/activate
 echo $VIRTUAL_ENV
 flask --debug run -h 0.0.0.0
