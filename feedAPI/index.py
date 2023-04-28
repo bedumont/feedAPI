d5cb5de2d4e80ef93e013d346db2aaee37aaee3d
Ben Dum <bedumont@ulb.ac.be>
2023-04-28 18:17:04 +0200

    Adapted index.py to use SQLAlchemy

--- feedAPI/index.py
+++ feedAPI/index.py
@@ -1,15 +1,54 @@
+import os
+from datetime import datetime
 from flask import Flask, jsonify, request
+
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, select
+
+from feedAPI.model.tables import Base
+from feedAPI.model.tables import Feedback
+from feedAPI.model.tables import Comment
 
 app = Flask(__name__)
 
-comment = [
-        {'ip': '0:0:0:0',
-         'text': 'This is a comment',
-         'rating': 3
-         }
-]
+# Some parameters, should end up in a file at some point
+FEEDBACK_FILE = "./feedbacks.json"
+REACTION_FILE = "./reactions.json"
+DATABASE = os.path.abspath("./database.db")
+DEBUG = True
+
+engine = create_engine("sqlite:///"+DATABASE, echo=DEBUG)
+
+# Ugly test to create db if we start from scratch
+if not os.path.isfile(DATABASE):
+    Base.metadata.create_all(engine)
 
 
 @app.route("/feedback")
-def hello_world():
-    return "Hello, World !"
+def get_feedbacks():
+    #session = Session(engine)
+    #stmt = select(Feedback)
+    #result = []
+    #for feedback in session.scalars(stmt):
+    #    print(feedback)
+    #_return "", 204
+    feedbacks = Feedback.query.all()
+    return jsonify(feedbacks)
+
+
+@app.route("/feedback", methods=['POST'])
+def post_feedback():
+    with Session(engine) as session:
+        feedback = Feedback(
+            source="0.0.0.0",
+            text="Awesome backend",
+            grade=5,
+            datetime=datetime.now()
+            )
+    session.add(feedback)
+    print("Adding "+feedback.__str__()+" to the DB")
+    session.commit()
+#    feedback = request.get_data(True,True,False)
+#    f = open(FEEDBACK_FILE, 'a')
+#    f.write(feedback)
+    return "", 204
