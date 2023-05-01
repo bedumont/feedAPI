630dea836fde9bea9f62a5411f9c4c5d711b8028
Ben Dum <bedumont@ulb.ac.be>
2023-05-01 16:22:28 +0200

    Added an __init__ function to the base table model to initialize an instance with an optional dict to provide data.
    Fixed docstring

--- feedAPI/model/tables.py
+++ feedAPI/model/tables.py
@@ -10,54 +10,99 @@ from sqlalchemy.orm import relationship
 
 
 class Base(DeclarativeBase):
+    """ Base table
+    """
     def as_dict(self):
-        # Return itself as a dict
+        """ Return table entry as dict
+        Used to jsonify the tables
+        """
         return {c.name: getattr(self, c.name)
                 for c in self.__table__.columns}
 
-
+    def __init__(self, data=None):
+        # Init from dict {columname: value}
+        """ Init an object instance from data in a dict
+        If no data provided create an instance with no data
+        """
+        if data is not None:
+            for key in data:
+                setattr(self, key, data[key])
 
 class Feedback(Base):
-    # Declarative description of the feedback table
+    """ Declarative description of the feedbacks table
+    Table has 6 fields:
+        id [int]: the primary key (assumed unique)
+        source [str]: the ip adress of the source
+        text [str, optional]: the text of the feedback
+        grade [int]: the grave of the feedback
+        score [int]: the sum of all upvotes and downvotes of the feedback
+        datetime [datetime]: the timestamp of when the feedback was sent (client side)
+    Inherit from Base
+    """
     __tablename__ = "feedbacks"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     source: Mapped[str] = mapped_column(String(15))
     text: Mapped[Optional[str]] = mapped_column(String(250))
     grade: Mapped[int] = mapped_column()
+    score: Mapped[int] = mapped_column(default=1)
     datetime: Mapped[datetime] = mapped_column(DateTime())
 
     def __repr__(self):
-        # Represents a feedback as a string for logging
+        """ Represents a feedback as a string for logging
+        """
         return '<Feedback(id={self.id} ip={self.source} datetime={self.datetime})>'.format(self=self)
 
 
 class Comment(Base):
-    # Declarative description of comments table
+    """ Declarative description of the comments table
+    Table has 6 fields:
+        id [int]: the primary key (assumed unique)
+        target [int]: the id of the feedback the entry is related to (ForeignKey)
+        source [str]: the ip adress of the source
+        text [str, optional]: the text of the comment
+        score [int]: the sum of all upvotes and downvotes of the comment
+        datetime [datetime]: the timestamp of when the comment was sent (client side)
+    Inherit from Base
+    """
     __tablename__ = "comments"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     target: Mapped[int] = mapped_column(ForeignKey("feedbacks.id"))
     source: Mapped[str] = mapped_column(String(15))
     text: Mapped[Optional[str]] = mapped_column(String(250))
-    score: Mapped[int] = mapped_column()
+    score: Mapped[int] = mapped_column(default=1)
     datetime: Mapped[datetime] = mapped_column(DateTime())
 
     def __repr__(self):
-        # Represents a comment as a string for logging
+        """ Represents a comment as a string for logging
+        """
         return '<Comment(id={self.id} source={self.ip} target={self.target} datetime={self.datetime})>'.format(self=self)
 
 
 class Reaction(Base):
-    # Declarative description of reactions table
+    """ Declarative description of the reactions table
+    Table has 6 fields:
+        id [int]: the primary key (assumed unique)
+        fb_id [int]: the id of the feedback the entry is related to (ForeignKey)
+            Null if the reaction is not related to a feedback
+        cmt_id [int]: the id of the comment the entry is related to (ForeignKey)
+            Null if the reaction is not related to a comment
+        source [str]: the ip adress of the source
+        value [int]: the value of the reaction (+1 or -1)
+        datetime [datetime]: the timestamp of when the reaction was sent (client side)
+    Inherit from Base
+    """
     __tablename__ = "reactions"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    target: Mapped[int] = mapped_column(ForeignKey("comments.id"))
+    fb_id: Mapped[Optional[int]] = mapped_column(ForeignKey("feedbacks.id"))
+    cmt_id: Mapped[Optional[int]] = mapped_column(ForeignKey("comments.id"))
     source: Mapped[str] = mapped_column(String(15))
-    value: Mapped[int] = mapped_column() #+1 -1 or 0
+    value: Mapped[int] = mapped_column()  # +1 -1 or 0
     datetime: Mapped[datetime] = mapped_column(DateTime())
 
     def __repr__(self):
-        # Represents a reaction as a string for logging
-        return '<Reaction(id={self.id} source={self.ip} target={self.target} datetime={self.datetime})>'.format(self=self)
+        """ Represents a reaction as a string for logging
+        """
+        return '<Reaction to feedback(id={self.id} source={self.ip} fb_id={self.fb_id} cmt_id={self.cmt_id} datetime={self.datetime})>'.format(self=self)
